f0b4b1ca47d842a85633c73d20fb2c1cefac9a25
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-08-21 12:36:19 -0700

    Add files via upload

---
+++ arrays/methods/array.every.js
@@ -0,0 +1,21 @@
+// use array.every() when you need to check if all elements in the array pass the test.
+
+// EXAMPLE: 
+const everyArray = [1, 2, 3, 4, 5];
+
+function isEven(element) {
+    return element % 2 === 0;
+}
+
+console.log(everyArray.every(isEven)); // false
+
+
+const task = [
+    { id: 1, title: 'Task 1', completed: true },
+    { id: 2, title: 'Task 2', completed: true },
+    { id: 3, title: 'Task 3', completed: false }
+];
+
+const completedTask = task.every(task => task.completed);
+
+console.log(completedTask); // false
---
+++ arrays/methods/array.find.js
@@ -0,0 +1,20 @@
+// array.find() is used to search for an element in the array that matches a specific condition.
+
+
+// the function and syntax of find() is very much like the 
+// array.filter method, except it only returns a single element. 
+
+// use find when you want a single element returned.
+// array.find() returns undefined if nothing is found. 
+
+//! use the filter instead of array.find when you need multiple elements returned.
+
+// EXAMPLE: 
+const trees = [
+    { name: "birch", count: 4 },
+    { name: "maple", count: 5 },
+    { name: "oak", count: 2 }
+];
+
+const result = trees.find(tree => tree.name === "birch");
+console.log(result); // {name: 'birch', count: 4}
