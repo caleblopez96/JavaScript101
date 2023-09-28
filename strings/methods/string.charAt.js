377cf1f5f3dff2b3e60496c49e4646119567f148
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-09-27 19:00:04 -0700

    Add files via upload

---
+++ strings/methods/string.charAt.js
@@ -0,0 +1,13 @@
+// use the charAt() methodto retrieve the character at a specified 
+// position (index) within a string.
+
+// EXAMPLE: 
+// returns the character at a specific index in a string.
+'Javascript'.charAt(2); // v
+function firstAndLastInitial(firstName, LastName) {
+    let firstIntial = firstName.charAt(0);
+    let lastInitial = lastName.charAt(0);
+    let final = `${firstIntial + lastInitial}`
+    return final
+}
+firstAndLastInitial('Caleb', 'Lopez');
