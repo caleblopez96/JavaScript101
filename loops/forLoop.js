d9812f9c45e04392fe2078fe11c8a54fed6ac7de
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-10-15 19:29:54 -0700

    Add files via upload

---
+++ loops/forLoop.js
@@ -0,0 +1,14 @@
+// for loops in JavaScript are used for iterating over sequences of data,
+// such as arrays and strings, or for performing a repetitive task a specific number of times.
+
+// EXAMPLE:
+// No loop:
+text += cars [0] + "<br>";
+text += cars [1] + "<br>";
+text += cars [2] + "<br>";
+
+// for loop
+for (let i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
+}
+
---
+++ loops/forOf.js
@@ -0,0 +1,18 @@
+// Use for...of loops to iterate thorugh the values of an iterable object:
+// loops over: arrays, strings, maps, nodelists
+
+// EXAMPLE: 
+
+// arrays
+const myArray = [1, 2, 3, 4];
+
+for (const element of myArray) {
+    console.log(element);
+}
+
+// strings
+const string = "Hello World";
+
+for (letter of string) {
+    console.log(letter);
+}
