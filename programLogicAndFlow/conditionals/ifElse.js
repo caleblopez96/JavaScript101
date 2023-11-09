cf7fcd70474fefef5a7b2f761b60dbe43795098b
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-11-08 17:14:50 -0700

    Delete programLogicAndFlow directory

--- programLogicAndFlow/conditionals/ifElse.js
+++
@@ -1,8 +0,0 @@
-const age = 15;
-
-if (age >= 18) {
-    console.log("You are an adult.");
-} else {
-    console.log("You are not an adult yet.");
-}
-
--- programLogicAndFlow/operators/unary/exponentiation.js
+++
@@ -1,4 +0,0 @@
-// returns the result of raising the first operand to the power of the second operand.
-
-// EXAMPLE: 
-console.log(3 ** 4); // 81
