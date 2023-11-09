f7515c693bb62b0f6c5a2b28a255312cdeb24451
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-11-08 17:12:32 -0700

    Delete programLogicAndFlow/comparisons directory

--- programLogicAndFlow/comparisons/greaterThan.js
+++
@@ -1,6 +0,0 @@
-// used to compare two values or expressions to check if the left operand is greater than the right operand.
-
-// EXAMPLE: 
-5 > 3;   // true, because 5 is greater than 3
-3 > 5;   // false, because 3 is not greater than 5
-5 > 5;   // false, because 5 is not greater than 5 (they are equal)
