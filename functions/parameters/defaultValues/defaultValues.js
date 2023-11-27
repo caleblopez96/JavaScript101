28b66234e5baddcff97f26726e92307750a04fef
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-11-27 00:13:48 -0700

    Add files via upload

---
+++ functions/parameters/defaultValues/defaultValues.js
@@ -0,0 +1,17 @@
+// default function parameters allow you to assign default values to parameters in a function declaration.
+
+// EXAMPLE:
+function guestGreeting(name = 'Guest') {
+    console.log(`Hello ${name}!`);
+}
+
+guestGreeting(); // Hello Guest!
+guestGreeting('John'); // Hello John!
+
+
+// EXAMPLE:
+function displayInfo( { name = 'User', age = '00' } ) {
+    console.log(`Hello: ${name}. Age: ${age}`);
+}
+
+displayInfo( { name: 'John Doe', age : 33 } );
