672ff750c26e688a517ad17fa293b795964182db
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-01-10 17:16:30 -0700

    Add files via upload

---
+++ functions/parameters/restOperator/defaultValues/defaultValues.js
@@ -0,0 +1,18 @@
+// default function parameters allow you to assign default values to parameters in a function declaration.
+
+// EXAMPLE:
+function guestGreeting(name = 'Guest') {
+    console.log(`Hello ${name}!`);
+}
+
+guestGreeting(); // Hello Guest!
+guestGreeting('John'); // Hello John!
+
+
+// EXAMPLE:
+function displayInfo( { name = 'User', age = '00' } ) {
+    console.log(`Hello: ${name}. Age: ${age}`);
+}
+
+displayInfo( { name: 'John Doe', age : 33 } ); // Hello: John Doe. Age: 33
+displayInfo({},{}); // Hello: User. Age: 00
---
+++ functions/parameters/restOperator/restOperator/restOperator.js
@@ -0,0 +1,33 @@
+// use the ...rest parameter to collect all remaining arguments into one single array. 
+
+// useful for functions that can take a variable number of arguments.
+
+// *PACKING elements into box*
+
+// EXAMPLE:
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+console.log(sum(1, 2, 3, 4, 5)); // 15
+
+// EXAMPLE: Combining multiple arrays into a single array
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+function combinedArrays(...arrays) {
+    return arrays.reduce((sum, num) => [...sum, ...num]);
+}
+const arr3 = combinedArrays(arr1, arr2);
+console.log(arr3); [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// EXAMPLE: Combine multiple objects into one array of objects
+const obj1 = {a: 1};
+const obj2 = {a: 2};
+
+function combineObjects(...objects) {
+    return objects.reduce((acc, obj) => {
+        return acc.concat(obj);
+    }, []);
+}
+const combinedObjects = combineObjects(obj1, obj2);
+console.log(combinedObjects); // [{…}, {…}]
