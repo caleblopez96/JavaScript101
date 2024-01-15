e47f6a656b7db0bfb1a83097ade32731d46718cc
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-01-14 23:30:15 -0700

    Add files via upload

---
+++ data/primitives.md
@@ -0,0 +1,10 @@
+// B U S N B N S ('bussin buns')
+
+// Boolean
+// Undefined
+// String
+// Number
+
+// BigInt
+// Null
+// Symbol
---
+++ data/variables/var/var.js
@@ -0,0 +1,9 @@
+// var - legacy keyword used to declare variables.
+
+// variables declared with var are function-scoped. 
+
+// variables declared with var can be reassinged. 
+
+var age = 26;
+age = 27;
+console.log(age);
