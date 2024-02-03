8eaa37347f9e6601a235d7baf8772245f0739c68
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-02-03 14:30:48 -0700

    Delete arrays directory

--- arrays/methods/array.fill.js
+++
@@ -1,8 +0,0 @@
-// array.fill() changes all elements with a range 
-
-// array.fill(value)
-// array.fill(value, start)
-// array.fill(value, start, end)
-
-const example = [1, 2, 3, 4, 5];
-example.fill('a', 3);
