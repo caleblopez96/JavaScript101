f70d2f12d2aab65f30df754f0b9b1e7cf852f432
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-03-23 20:26:42 -0700

    Delete data directory

--- data/objects/objectMethods/object.assign.js
+++
@@ -1,9 +0,0 @@
-// used to copy the values of all enumerable properties from one or more source objects to a target object.
-
-const source = { name: "john", age: 30 };
-
-const target = {};
-
-Object.assign(target, source);
-
-console.log(target); // {name: 'john', age: 30}
--- data/variables/let/let.js
+++
@@ -1,9 +0,0 @@
-// let: a keyword in javascript used to declare variables with block scope.
-
-let color = 'red';
-
-// the value of let can be reassigned. 
-
-color = 'purple';
-
-console.log(color); // purple
