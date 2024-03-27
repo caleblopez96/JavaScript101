8799d328751ac89d75a78677155510edceaf83fd
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-03-27 00:51:38 -0700

    Delete Functions directory

--- Functions/functionExpression.js
+++
@@ -1,9 +0,0 @@
-// Function expressions create a function and assign it to a variable. 
-// Unlike function declarations, function expressions are not hoisted.
-
-// EXAMPLE:
-const addition = function(...numbers) {
-    return numbers.reduce((sum, num) => sum + num);
-}
-
-console.log(addition(1, 2, 3, 4)); // 10
