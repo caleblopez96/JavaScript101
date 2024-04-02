bf865e5caa4dc593bfae0327f71c4f262033f527
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-04-02 01:23:36 -0700

    Add files via upload

---
+++ Document-Object-Model/Events/eventHandlers/submit.js
@@ -0,0 +1,12 @@
+// the submit event fires when a form is submitted
+
+// SYNTAX:
+// addEventListener('submit', (event) => {});
+
+// EXAMPLE:
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevents default event of submit
+});
