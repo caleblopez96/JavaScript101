5e99b906c522bd5027a37254121ff69777c7b32a
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-06-24 20:39:26 -0700

    Add files via upload

--- Web-APIs/fetch/fetch.js
+++ Web-APIs/fetch/fetch.js
@@ -1,26 +1,53 @@
 // The Fetch API is used to make HTTP requests.
+// Fetch is promise based; it uses the .then() and the .catch() methods.
 
+// SYNTAX:
+// fetch()
+// fetch(url, {options})
+
+// EXAMPLE:
+// const url = 'https://api.example.com/data';
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log('Error: ', error));
+
+
+
+// RESOURCE:
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-const url = 'https://api.example.com/data';
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .then(error => console.log('Error: ', error));
+const tarkovAPI = 'https://api.tarkov.dev/graphql';
 
-// EXAMPLE of fetch with dad joke api
-function getJoke() {
-    fetch("https://icanhazdadjoke.com/", {
-        headers: {
-        Accept: "application/json"
-        }
-    })
-        .then(function (response) {
-        return response.json();
-    })
-        .then(function (data) {
-        const joke = data.joke;
-        jokeText.innerText = `${joke}😂`;
-    });
-}
+
+fetch(tarkovAPI, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({query: `{
+    items(name: "glasses") {
+      id,
+      name,
+      shortName,
+      iconLink
+    }  
+  }`})
+})
+.then(response => response.json())
+.then(data => {
+  if (data.data.items && data.data.items.length > 0) {
+    const testDiv = document.querySelector(".test");
+    let itemNames = ''; // Initialize an empty string to accumulate item names
+
+    for (let i = 0; i < data.data.items.length; i++) {
+      console.log(data.data.items[i].name);
+      itemNames += data.data.items[i].name;
+    }
+    testDiv.textContent = itemNames;
+  }
+})
+.catch(error => console.log(error))
