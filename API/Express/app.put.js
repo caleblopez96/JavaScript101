db4c2b6c73fd0878119181c401b33e7a7ff7a594
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-08-22 08:42:35 -0700

    Add files via upload

--- API/Express/app.put.js
+++ API/Express/app.put.js
@@ -2,4 +2,15 @@
 
 // updating an existing book: PUT /api/books{id}
 
-app
+app.put('/api/books:id', (res, rej) => {
+  Book.findByIdUpdate(res.params.id, res.body, { new: true })
+  .then(updatedBook => {
+    if(!updatedBook) {
+      return res.status(404).json({ error: 'Book not found' })
+    }
+    res.json(updatedBook)
+  })
+  .catch(err => {
+    res.status(404).json({ error: 'Failed to update book, please try again' })
+  })  
+})
