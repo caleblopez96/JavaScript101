6c65a79fc157db4052b30b8789b6f29cc52831f4
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-08-29 23:48:05 -0700

    Add files via upload

--- Data/arrays/methods/callbackMethods/array.forEach.js
+++ Data/arrays/methods/callbackMethods/array.forEach.js
@@ -28,3 +28,24 @@ shoppingCart.forEach(function(item){
 
 console.log(`Total Price: $${totalPrice.toFixed(2)}`); // $1222.00
 
+
+
+
+// EXAMPLE: 
+// Write a JavaScript program to find the 
+// most frequent item in an array.
+function findMostFrequent(array) {
+  let count = {}
+
+  array.forEach(item => {
+    if (count[item]) {
+      count[item] += 1
+    } else {
+      count[item] = 1
+    }
+  })
+  console.log(count)
+}
+
+let arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+findMostFrequent(arr1) // {2: 2, 3: 4, 4: 1, 9: 1, a: 5}
