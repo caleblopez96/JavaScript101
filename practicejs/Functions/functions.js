11182a8e6e4c5ca1a0d0f7d3ec74952e2a34b6bb
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-09-02 10:00:36 -0700

    Add files via upload

--- practicejs/Functions/functions.js
+++ practicejs/Functions/functions.js
@@ -8,7 +8,7 @@ console.log(reverseNumber(512)); // 215
 
 
 
-
+//* COMPLETE
 // 4. Write a JavaScript function that returns a string that has letters in alphabetical order.
 function alphabetize(string) {
   return Array.from(string).sort().join('')
@@ -30,3 +30,75 @@ function capitalizeTheFirstLetter(string){
   return capitalizeLetters.join('')
 }
 capitalizeTheFirstLetter('hello there')
+/////////////////////////////////////////////
+
+
+
+
+//* COMPLETE
+// 6. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
+function longestWord(string) {
+
+  let wordArray = string.split(" ")
+
+  let longestWord = ''
+
+  wordArray.forEach(word => {
+    if(word.length > longestWord.length) {
+      longestWord = word
+    }
+  });
+  return longestWord
+}
+longestWord('test testing') // 'testing'
+/////////////////////////////////////////////
+
+
+
+
+//* COMPLETE
+// 7. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+function vowelCounter(string) {
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let count = 0
+
+  let wordArray = string.toLowerCase().split("")
+  wordArray.forEach(letter => {
+    if(vowels.includes(letter)){
+      count++
+    }
+  })
+  return count
+}
+vowelCounter('aeiom') // 4
+/////////////////////////////////////////////
+
+
+
+
+//* COMPLETE
+// 16. Write a JavaScript function to extract unique characters from a string.
+function extractUniqueCharacters(string) {
+  let uniqueCharacters = new Set(string)
+  return [...uniqueCharacters].join('')
+}
+
+console.log(extractUniqueCharacters("thequickbrownfoxjumpsoverthelazydog")) //thequickbrownfxjmpsvlazydg
+
+
+
+
+
+// Write a JavaScript function to get the number of occurrences of each letter in a specified string.
+
+
+
+
+
+
+// Write a JavaScript function that returns array elements larger than a number.
+function elLargerThan(array, n) {
+  return array.filter(element => element > n)
+}
+
+elLargerThan([1,2,3],1) // [2, 3]
