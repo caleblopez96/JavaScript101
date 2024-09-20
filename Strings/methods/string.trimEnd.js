082ca94ebc892125c8164994941140d32721b969
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-09-19 22:57:04 -0700

    Delete Strings directory

--- Strings/methods/string.trimEnd.js
+++
@@ -1,7 +0,0 @@
-// used to remove whitespace characters from the end of a string. 
-
-// EXAMPLE:
-const text = "Hello, world!   ";
-const trimmedText = text.trimEnd();
-
-console.log(trimmedText); // Output: "Hello, world!"
--- Strings/methods/string.trimStart.js
+++
@@ -1,7 +0,0 @@
-// used to remove whitespace characters from the beginning (start) of a string.
-
-// EXAMPLE:
-const text = "   Hello, world!";
-const trimmedText = text.trimStart();
-
-console.log(trimmedText); // Output: "Hello, world!"
