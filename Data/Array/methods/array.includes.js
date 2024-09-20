d29460f6371c67fb2215d3f6bb08023bd426f6ec
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-09-19 22:39:22 -0700

    Add files via upload

---
+++ Data/Array/methods/array.includes.js
@@ -0,0 +1,18 @@
+// checks if array includes a certain element or value.
+// does NOT mutate original array.
+
+
+// SYNTAX: 
+// includes(searchElement)
+// includes(searchElement, fromIndex)
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+// EXAMPLE:
+const schoolSubjects = [ "Math", "Science", "Social Studies" ];
+
+let mathSubjectCheck = schoolSubjects.includes("Math");
+console.log(mathSubjectCheck); // true
+
+let artSubjectCheck = schoolSubjects.includes("Art");
+console.log(artSubjectCheck); // false
