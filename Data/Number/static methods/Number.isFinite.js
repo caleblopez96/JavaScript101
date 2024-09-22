b7a99d7172388663999f0213d1706b38aa8ef000
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-09-22 12:08:55 -0700

    Add files via upload

---
+++ Data/Number/static methods/Number.isFinite.js
@@ -0,0 +1,9 @@
+// you can use Number.isFinite() to determine if a number passed 
+// is a finite value
+
+// SYNTAX:
+// Number.isFinite(value)
+
+// EXAMPLE:
+console.log(Number.isFinite(1 / 0)) // false
+console.log(Number.isFinite(2 / 10)) // true
---
+++ Data/Number/static methods/Number.isInteger.js
@@ -0,0 +1,8 @@
+// use Number.isInteger() to determine if number is integer
+
+// SYNTAX:
+// Number.isInteger(value)
+
+// EXAMPLE:
+console.log(Number.isInteger(3)) // true
+console.log(Number.isInteger('a')) // false
