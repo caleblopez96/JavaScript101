5323fad5c3f7eb12121f4275c62e404f0806b2b0
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-09-22 11:50:31 -0700

    Add files via upload

---
+++ Data/Number/functions/Number.isNan.js
@@ -0,0 +1,8 @@
+// use Number.isNan() to determine if value if NaN
+
+// SYNTAX: 
+// Number.isNaN(value)
+
+// EXAMPLE:
+console.log(Number.isNaN(2)) // false
+console.log(Number.isNaN(NaN)) // true
---
+++ Data/Number/functions/Number.parseInt.js
@@ -0,0 +1,7 @@
+// use Number.parseInt() to parse a string and return an int
+
+// SYNTAX: 
+// Number.parseInt(string) 
+
+// EXAMPLE:
+console.log(Number.parseInt('7')) // 7
