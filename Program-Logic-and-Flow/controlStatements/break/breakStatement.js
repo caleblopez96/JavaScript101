55cf3b4373c9f60952e5499d50b6b5cb0d209f02
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-09-28 20:51:51 -0700

    Delete Program-Logic-and-Flow directory

--- Program-Logic-and-Flow/controlStatements/break/breakStatement.js
+++
@@ -1,8 +0,0 @@
-// use a break statement to terminate the loop immediately upon the break statement.
-
-for(let i = 1; i <=5; i++) {
-    if(i === 3) {
-        console.log('test');
-        break;
-    }
-}
--- Program-Logic-and-Flow/operators/unary/delete.js
+++
@@ -1,5 +0,0 @@
-// Delete Operator: Deletes a property from an object, or an element from an array.
-
-let obj = { key: "value" };
-delete obj.key; // prop is removed from obj
-console.log(obj); // {}
