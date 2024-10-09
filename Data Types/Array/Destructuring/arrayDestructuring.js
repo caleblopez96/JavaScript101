76f2eacfd120eb2ab732bd048f192fa74a980c5e
caleblopez96 <caleblopez96@gmail.com>
2024-10-09 15:34:26 -0700

    fixed skipping element syntax

--- Data Types/Array/Destructuring/arrayDestructuring.js
+++ Data Types/Array/Destructuring/arrayDestructuring.js
@@ -6,18 +6,19 @@
 const fruits = ['Apple', 'Banana', 'Orange']
 
 const [firstFruit, secondFruit, thirdFruit] = fruits
-console.log(firstFruit) //
-console.log(secondFruit) //
-console.log(thirdFruit) //
+console.log(firstFruit) // Apple
+console.log(secondFruit) // Banana
+console.log(thirdFruit) // Orange
 
 
-// Destructuring and using rest to collect rest of values
+// Destructuring using rest to collect rest of values
 const numbers = [1, 2, 3, 4, 5]
 
 const [firstNum, secondNum, ...remainingNums] = numbers
 console.log(firstNum) // 1
 console.log(secondNum) // 2
 console.log(remainingNums) // [3, 4, 5]
+
 
 // destructuring by skipping elements
 const students = ['john', 'alice', 'jim', 'stan']
