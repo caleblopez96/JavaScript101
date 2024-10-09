0d583b33c6384d7f51b0f424c5b9de97d876bc07
caleblopez96 <caleblopez96@gmail.com>
2024-10-09 15:27:45 -0700

    fixed bugged file

---
+++ Data Types/Array/Destructuring/arrayDestructuring.js
@@ -0,0 +1,28 @@
+// use array destructuring to unpack values from an array
+
+// EXAMPLE:
+// extract the values from the fruits array and store them to a variable
+
+const fruits = ['Apple', 'Banana', 'Orange']
+
+const [firstFruit, secondFruit, thirdFruit] = fruits
+console.log(firstFruit) //
+console.log(secondFruit) //
+console.log(thirdFruit) //
+
+
+// Destructuring and using rest to collect rest of values
+const numbers = [1, 2, 3, 4, 5]
+
+const [firstNum, secondNum, ...remainingNums] = numbers
+console.log(firstNum) // 1
+console.log(secondNum) // 2
+console.log(remainingNums) // [3, 4, 5]
+
+// destructuring by skipping elements
+const students = ['john', 'alice', 'jim', 'stan']
+
+const [student1, student2, , student4] = students
+console.log(student1) // john
+console.log(student2) // alice
+console.log(student4) // stan
