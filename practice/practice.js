e3bdee3942f20f88394791af851e66e00ec5766b
caleblopez96 <caleblopez96@gmail.com>
2024-10-12 16:04:23 -0700

    new problems added

--- practice/practice.js
+++ practice/practice.js
@@ -1,12 +1,2 @@
-function isPalindrome(string) {
-  return string.split('').reverse().join('') === string
-}
-console.log(isPalindrome('racecar'))
-console.log(isPalindrome('taco'))
 
-// function capitalizeWords(string) {
-//   return string
-//   .split('')
-//   .map(word => word.charAt(0).toUpperCase())
-// }
 
