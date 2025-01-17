abc3eb92832ad7577e2e98f1220199093b0b109b
caleblopez96 <caleblopez96@gmail.com>
2025-01-16 17:47:07 -0700

    completed more practice questions

---
+++ practice/Square a Number/squareANumber.js
@@ -0,0 +1,8 @@
+// Square a Number
+// Write a function that takes a number and returns its square(number multiplied by itself).
+
+function squareNumber(number) {
+  return number * number
+}
+
+console.log(squareNumber(4)) // 16
