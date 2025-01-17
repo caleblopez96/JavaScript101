fccdef62ba79bfedd77d177994a7149098cc9157
caleblopez96 <caleblopez96@gmail.com>
2025-01-16 19:17:16 -0700

    more practice questions

---
+++ practice/Count Elements in Array/countElementsInArray.js
@@ -0,0 +1,9 @@
+// Count the Number of Elements in an Array
+// Write a function that takes an array and returns the number of elements in that array.
+
+function countElementsInArray(array) {
+  return array.length
+}
+
+const numberArray = [1, 2, 3, 4, 5, 6]
+console.log(countElementsInArray(numberArray)) //
