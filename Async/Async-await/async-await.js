f9a1eb77bdaaa6fd6597cb44bdb68f98a1f5f8b2
caleblopez96 <caleblopez96@gmail.com>
2025-01-16 23:04:23 -0700

    see last commit message

--- Async/Async-await/async-await.js
+++ Async/Async-await/async-await.js
@@ -4,27 +4,28 @@
 
 // you cannot use await without async.
 
-// 
-
-// Without ASYNC...AWAIT
-async function fetchData(){
-  return fetch('https://example.com/data')
-  .then((response) => response.json)
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error))
-}
-
 // With ASYNC...AWAIT
 async function fetchData() {
   try {
     const response = await fetch('https://swapi.dev/api/planets/')
     const data = await response.json()
-    for(let planet of data.results) {
+    for (let planet of data.results) {
       console.log(planet.name)
     }
-} catch(error) {
+  } catch (error) {
     console.log(error)
   }
+}
+
+// Without ASYNC...AWAIT
+async function fetchData() {
+  return fetch('URL_HERE')
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle data here
+      console.log(data);
+    })
+    .catch((error) => console.log(error)); // Handle errors
 }
 
 fetchData();
