aec16b0d53df694441e8708fc7d1fcd2cf701843
caleblopez96 <caleblopez96@gmail.com>
2025-01-24 19:33:11 -0700

    formatting

--- Functions/Function Parameters/defaultParameters.js
+++ Functions/Function Parameters/defaultParameters.js
@@ -1,11 +1,8 @@
 // default function parameters allow you to 
-// assign default values to parameters in a 
-// function declaration.
+// assign default values to parameters in a function declaration.
 
 // SYNTAX: 
-//function fnName(param1 = defaultValue1) {
-
-// }
+// function fnName(param = defaultValue) {}
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters
 
@@ -13,7 +10,6 @@
 function guestGreeting(name = 'Guest') {
   return console.log(`Hello ${name}!`);
 }
-
 guestGreeting(); // Hello Guest!
 guestGreeting('John'); // Hello John!
 
@@ -22,6 +18,5 @@ guestGreeting('John'); // Hello John!
 function displayInfo({ name = 'User', age = '00' }) {
   return console.log(`Hello: ${name}. Age: ${age}`);
 }
-
 displayInfo({ name: 'John Doe', age: 33 }); // Hello: John Doe. Age: 33
 displayInfo({}, {}); // Hello: User. Age: 00
