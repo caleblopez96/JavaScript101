76119327d11a31da0cb34b1453d9d40461e8dffc
caleblopez96 <caleblopez96@gmail.com>
2025-01-24 19:14:57 -0700

    added information about hoisting for var variable declarations

--- Variables/var/var.js
+++ Variables/var/var.js
@@ -1,9 +1,10 @@
 // var - legacy keyword used to declare variables.
 
 // variables declared with var are function-scoped. 
+// variables with var are hoisted which can cause unexpected behaviors.
 
 // variables declared with var can be reassigned. 
 
 var age = 26;
 age = 27;
-console.log(age);
+console.log(age); // 27
