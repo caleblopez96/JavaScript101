f6dd4d96b4e024d0af603b138e1365e4bba41a47
caleblopez96 <caleblopez96@gmail.com>
2025-02-08 12:13:19 -0700

    added practice question for classes

---
+++ Program Logic and Flow/Classes/Practice/Bank Account/bankAccount.js
@@ -0,0 +1,35 @@
+class BankAccount {
+  constructor(accountNumber, accountHolder, balance = 0) {
+    this.accountNumber = accountNumber;
+    this.accountHolder = accountHolder;
+    this.balance = balance;
+  }
+
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+      console.log(`You deposited $${amount}. New balance is: ${this.balance}`);
+    } else {
+      console.log(`Can't deposit a negative amount.`);
+    }
+  }
+
+  withdraw(amount) {
+    if (amount > this.balance) {
+      console.log(`Insufficient Funds`);
+    } else {
+      this.balance -= amount;
+      console.log(
+        `You withdrew $${amount}. Your new balance is $${this.balance}`
+      );
+    }
+  }
+}
+
+const a = new BankAccount("123", "James Dean");
+
+console.log(a); // BankAccount {accountHolder: James Dean, accountNumber: 123}
+a.deposit(200);
+console.log(a); // BankAccount {accountHolder: James Dean, accountNumber: 123, balance: 200}
+a.withdraw(100);
+console.log(a); // BankAccount {accountHolder: James Dean, accountNumber: 123, balance: 100}
