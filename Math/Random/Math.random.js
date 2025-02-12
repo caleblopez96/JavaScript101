4ef1bcfbcfde6d29e4aad9c852f162edeca71594
caleblopez96 <caleblopez96@gmail.com>
2025-02-12 10:01:45 -0700

    added example to math.random for clarification

--- Math/Random/Math.random.js
+++ Math/Random/Math.random.js
@@ -1,27 +1,48 @@
-// The Math.random() static method returns a
-// floating-point, pseudo-random number that's 
-// greater than or equal to 0 and less than 1, 
-// with approximately uniform distribution over that 
-// range — which you can then scale to your desired range.
+// use the .random() method on the Math object to get a random number
 
 // SYNTAX:
 // Math.random()
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 // EXAMPLE:
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-console.log(getRandomInt(3));
-// Expected output: 0, 1 or 2
+console.log(getRandomInt(3)); // 0, 1 or 2
 
-console.log(getRandomInt(1));
-// Expected output: 0
+console.log(getRandomInt(1)); // 0
 
-console.log(Math.random());
-// Expected output: a number from 0 to <1
+console.log(Math.random()); // a number from 0 to <1
 
-// EXAMPLE: 
-console.log(Math.floor(Math.random() * 10)) //
+// EXAMPLE:
+console.log(Math.floor(Math.random() * 10)); // random number between 1 - 10
+
+// EXAMPLE: get random index from array
+function randomInd(array) {
+  let rand = Math.floor(Math.random() * array.length);
+  let randomIndex = array[rand];
+  return randomIndex;
+}
+
+const carArray = [
+  "Ford",
+  "Chevrolet",
+  "GMC",
+  "Cadillac",
+  "Buick",
+  "Dodge",
+  "Chrysler",
+  "Jeep",
+  "Ram",
+  "Tesla",
+  "Lincoln",
+  "Rivian",
+  "Lucid",
+  "Hennessey",
+  "Saleen",
+];
+
+console.log(randomInd(carArray));
+
+// REFERENCE
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
