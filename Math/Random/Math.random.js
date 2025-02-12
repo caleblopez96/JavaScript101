3890370c82d829d8a0ec69f957f8b6ca763403f2
caleblopez96 <caleblopez96@gmail.com>
2025-02-12 10:08:11 -0700

    deleted duplicate file

--- Math/Random/Math.random.js
+++ Math/Random/Math.random.js
@@ -3,21 +3,18 @@
 // SYNTAX:
 // Math.random()
 
-// EXAMPLE:
+// EXAMPLE 1: Random number 1 - 10
+console.log(Math.floor(Math.random() * 10)); // random number between 1 - 10
+
+// EXAMPLE 2: function that takes in a max number and picks a random number between 0 and that number
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-
 console.log(getRandomInt(3)); // 0, 1 or 2
-
 console.log(getRandomInt(1)); // 0
-
 console.log(Math.random()); // a number from 0 to <1
 
-// EXAMPLE:
-console.log(Math.floor(Math.random() * 10)); // random number between 1 - 10
-
-// EXAMPLE: get random index from array
+// EXAMPLE 3: get random index from array
 function randomInd(array) {
   let rand = Math.floor(Math.random() * array.length);
   let randomIndex = array[rand];
