7eafa8f580f0b8d40fe31de4ade099fe97962471
caleblopez96 <caleblopez96@gmail.com>
2025-02-12 10:17:38 -0700

    clarified examples in spread

--- Data Types/Objects/Object/Spread/spread.js
+++ Data Types/Objects/Object/Spread/spread.js
@@ -1,11 +1,23 @@
-// use the spread operator to unpack values of an object
+// use the spread operator with objects to make a copies of an object or combine the properties of objects
 
 // SYNTAX:
-// ...array
+// {...obj}
 
-// EXAMPLE:
-const obj = {
-  a: 1,
-  b: 2,
+// EXAMPLE 1: making a copy of an object
+const obj = { a: 1, b: 2 };
+const objCopy = { ...obj };
+console.log(objCopy); // {a: 1, b:2}
+
+// EXAMPLE 1: combine the properties of objects
+const person = {
+  firstName: "Caleb",
+  age: 28,
 };
-console.log({ ...obj }); // {a: 1, b: 2}
+
+const personDetails = {
+  location: "Arizona",
+  Occupation: "Software Developer",
+};
+
+const personWithDetails = { ...person, ...personDetails };
+console.log(personWithDetails); // { "firstName": "Caleb", "age": 28, "location": "Arizona", "Occupation": "Software Developer"}
