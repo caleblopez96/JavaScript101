ff557dc66bb7da7f5f7f2c2f38bde512d8957371
caleblopez96 <caleblopez96@gmail.com>
2025-02-18 17:50:58 -0700

    added syntax, params, and return info

--- Functions/Instance Methods/Bind/function.bind.js
+++ Functions/Instance Methods/Bind/function.bind.js
@@ -1,5 +1,19 @@
 // .bind() permanently binds a function to a specific object.
 
+// SYNTAX:
+// .bind(thisArg);
+// .bind(thisArg, arg1);
+// .bind(thisArg, arg1, arg2, /* …, */ argN);
+
+// PARAMETERS:
+// thisArg - the value (typically an object) to use as this when calling the function
+// arg1, ..., argN (optional) - arguments to the function
+
+// RETURNS:
+// copy of the function with the specified this value and arguments
+
+// EXAMPLE:
+
 const conan = {
   name: "Conan",
   city: "Los Angeles",
