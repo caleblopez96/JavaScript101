5fb35c9ed9c5d80dd158e34f8a5ba7b5b3e1f97e
caleblopez96 <caleblopez96@gmail.com>
2025-02-22 16:15:45 -0700

    clarifying object methods and converting to template format

--- Data Types/Non-Primitives/Object/Static Methods/Object.assign.js
+++ Data Types/Non-Primitives/Object/Static Methods/Object.assign.js
@@ -1,22 +1,34 @@
-// use Object.assign() to copy the values and props
-// from one source object to a target object
+// .assign() copies properties from one or more source objects to a target object
 
 // SYNTAX:
 // Object.assign(target, ...sources)
 
+// PARAMETERS:
+// target - the object to receive properties
+// sources - one or more source objects whose props are copied
+
+// RETURNS:
+// The modified target object
+
+// MUTATION:
+// Mutates the target object
+
 // EXAMPLE:
+
 const obj1 = {
-  a: 'a',
-  b: 'b',
-  c: 'c'
-}
+  a: "first",
+  b: "second",
+  c: "third",
+};
 
-const obj2 = {
-  d: 'd',
-  e: 'e',
-  f: 'f'
-}
+// Create a shallow copy of obj1
+const obj1Copy = Object.assign({}, obj1);
+console.log(obj1Copy); // { a: 'first', b: 'second', c: 'third' }
 
-// Combine objects to create new object
-const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3) // { a: 'a', b: 'b', c: 'c', d: 'd', e: 'e','f' }
+// Combine objects to create a new object
+const obj2 = { d: "fourth", e: "fifth", f: "sixth" };
+const obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3); // { a: 'first', b: 'second', c: 'third', d: 'fourth', e: 'fifth', f: 'sixth' }
+
+// REFERENCE:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
---
+++ Data Types/Non-Primitives/Object/Static Methods/Object.keys.js
@@ -0,0 +1,28 @@
+// .keys() returns an array of an object's own enumerable property keys
+
+// SYNTAX:
+// Object.keys(object)
+
+// PARAMETERS:
+// object - the object to retrieve keys from
+
+// RETURNS:
+// An array containing the object's keys
+
+// MUTATION:
+// does NOT mutate the original object
+
+// EXAMPLE:
+const object1 = {
+  a: "somestring",
+  b: 42,
+  c: false,
+};
+
+const object1Keys = Object.keys(object1);
+console.log(object1Keys); // ['a', 'b', 'c']
+
+console.log(Object.keys(object1)); // ['a', 'b', 'c']
+
+// REFERENCE:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
