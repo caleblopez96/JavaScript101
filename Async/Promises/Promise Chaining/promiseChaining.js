07168a8c3aa243528ff924d4dff665d3c9f4fc10
caleblopez96 <caleblopez96@gmail.com>
2025-02-24 17:13:53 -0700

    clarified examples on promise chaining

--- Async/Promises/Promise Chaining/promiseChaining.js
+++ Async/Promises/Promise Chaining/promiseChaining.js
@@ -5,58 +5,75 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const url = `${BASE_URL}`;
 
+// BEST WAY - ASYNC...AWAIT with TRY...CATCH
+async function getFourPokemon() {
+  try {
+    const res1 = await fetch(`${url}/1`);
+    const pokemon1 = await res1.json();
+    console.log(res1);
+
+    const res2 = await fetch(`${url}/2`);
+    const pokemon2 = await res2.json();
+    console.log(res2);
+
+    const res3 = await fetch(`${url}/3`);
+    const pokemon3 = await res3.json();
+    console.log(res3);
+
+    const res4 = await fetch(`${url}/4`);
+    const pokemon4 = await res4.json();
+    console.log(res4);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+getFourPokemon();
+
 // WRONG WAY - NESTED PROMISES = CALLBACK HELL:
 fetch(`${url}/1`)
-  .then((res1) => {
-    console.log("response 1: ", res1);
+  .then((res1) => res1.json()) // Parse JSON response
+  .then((data1) => {
+    console.log("Response 1:", data1);
     fetch(`${url}/2`)
-      .then((res2) => {
-        console.log("response 2: ", res2);
+      .then((res2) => res2.json())
+      .then((data2) => {
+        console.log("Response 2:", data2);
         fetch(`${url}/3`)
-          .then((res3) => {
-            console.log("Response 3: ", res3);
+          .then((res3) => res3.json())
+          .then((data3) => {
+            console.log("Response 3:", data3);
             fetch(`${url}/4`)
-              .then((res4) => {
-                console.log("Response 4: ", res4);
+              .then((res4) => res4.json())
+              .then((data4) => {
+                console.log("Response 4:", data4);
               })
-              .catch((err) => console.log("Error: ", err));
+              .catch((err) => console.log("Error:", err));
           })
-          .catch((err) => console.log("Error: ", err));
+          .catch((err) => console.log("Error:", err));
       })
-      .catch((err) => console.log("Error: ", err));
+      .catch((err) => console.log("Error:", err));
   })
-  .catch((err) => console.log("Error: ", err));
+  .catch((err) => console.log("Error:", err));
 
 // OKAY WAY - avoiding callback hell by returning promises instead of nesting them:
 fetch(`${url}/1`)
-  .then((res1) => {
-    console.log("Response 1: ", res1);
+  .then((res1) => res1.json()) // Parse JSON response
+  .then((data1) => {
+    console.log("Response 1:", data1);
     return fetch(`${url}/2`);
   })
-  .then((res2) => {
-    console.log("Response 2: ", res2);
+  .then((res2) => res2.json())
+  .then((data2) => {
+    console.log("Response 2:", data2);
     return fetch(`${url}/3`);
   })
-  .then((res3) => {
-    console.log("Response 3: ", res3);
+  .then((res3) => res3.json())
+  .then((data3) => {
+    console.log("Response 3:", data3);
     return fetch(`${url}/4`);
   })
-  .then((res4) => {
-    console.log("Response 4: ", res4);
+  .then((res4) => res4.json())
+  .then((data4) => {
+    console.log("Response 4:", data4);
   })
-  .catch((err) => console.log("Error: ", err));
-
-// BEST WAY - ASYNC...AWAIT with TRY...CATCH
-async function getFourPokemon() {
-  const res1 = await fetch(`${url}/1`);
-  console.log(res1);
-
-  const res2 = await fetch(`${url}/2`);
-  console.log(res2);
-
-  const res3 = await fetch(`${url}/3`);
-  console.log(res3);
-
-  const res4 = await fetch(`${url}/4`);
-  console.log(res4);
-}
+  .catch((err) => console.log("Error:", err));
