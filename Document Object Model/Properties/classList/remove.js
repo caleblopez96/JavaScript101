b2d9e12df87b75f69d1861904073bb2120059604
caleblopez96 <caleblopez96@gmail.com>
2025-02-26 11:23:55 -0700

    moved a lot of files around; they werent in the right spot

--- Document Object Model/Properties/classList/remove.js
+++
@@ -1,6 +0,0 @@
-// use classList.remove() to remove one or more classes to an element. 
-
-// EXAMPLE:
-const element = document.querySelector('element');
-element.classList.add('active', 'highlight');
-element.classList.remove('active');
