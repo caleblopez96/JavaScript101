3ee0ec11a402b93cdd87799d322e824be34f4008
caleblopez96 <caleblopez96@gmail.com>
2025-02-27 17:09:09 -0700

    clarified classList.add

--- Document Object Model/Modifying Element Classes/classList/add/classList.add.js
+++ Document Object Model/Modifying Element Classes/classList/add/classList.add.js
@@ -1,5 +1,14 @@
-// use classList.add() to add one or more classes to an element. 
+// use the .add method to add a classlist to an HTML element
+
+// SYNTAX:
+// element.classList.add("className")
+
+// PARAMETERS:
+// className - name of the class you want to add to the elements class list.
 
 // EXAMPLE:
-const element = document.querySelector('element');
-element.classList.add('active', 'highlight');
+const dropdown = document.querySelector(".dropdown");
+dropdown.classList.add("active");
+
+// REFERENCE:
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
