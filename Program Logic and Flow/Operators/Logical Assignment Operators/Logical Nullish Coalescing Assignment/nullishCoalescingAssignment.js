7e351401e4d1f3cc1bccaa3b638b3caae8edadb4
caleblopez96 <caleblopez96@gmail.com>
2025-03-01 15:07:42 -0700

    added a better example and references

--- Program Logic and Flow/Operators/Logical Assignment Operators/Logical Nullish Coalescing Assignment/nullishCoalescingAssignment.js
+++ Program Logic and Flow/Operators/Logical Assignment Operators/Logical Nullish Coalescing Assignment/nullishCoalescingAssignment.js
@@ -1,4 +1,4 @@
-// assigns a value to the right side only if the right side is null or undefined
+// The Nullish Coalescing (??=) assignment operator assigns a value to the right side only if the left side is null or undefined
 
 // SYNTAX:
 // x ??= y
@@ -8,9 +8,25 @@
 // }
 
 // EXAMPLE:
-
 let x = undefined;
 let y = 10;
 
 x ??= y;
 console.log(x); // 10
+
+// EXAMPLE:
+function doSomeSetup(options = {}) {
+  options.timeout ??= 3000; // if this property is left null or undefined, default it to 3 seconds
+  options.retries ??= 5; // if this property is left null or undefined, default it to 5
+  return options;
+}
+let setup1 = doSomeSetup();
+console.log(setup1); // {retries: 5, timeout: 3000}
+
+// REFERENCES:
+
+// COLT STEELE:
+// https://www.udemy.com/course/pro-javascript/learn/lecture/40741338#overview
+
+// MDN:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_assignment
