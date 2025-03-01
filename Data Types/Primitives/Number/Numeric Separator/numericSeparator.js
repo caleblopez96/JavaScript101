f9c3bd8f1e5d0d25083b8177a0049b80399b1897
caleblopez96 <caleblopez96@gmail.com>
2025-02-28 18:29:20 -0700

    misc changes

---
+++ Data Types/Primitives/Number/Numeric Separator/numericSeparator.js
@@ -0,0 +1,9 @@
+// numeric separators makes large numbers more readable by allowing underscores (_) as visual separators.
+
+// does not affect the actual value of the number.
+
+let num = 9_000_000;
+console.log(num); // 9000000
+
+let bigNumber = 1_234_567_890;
+console.log(bigNumber); // 1234567890
