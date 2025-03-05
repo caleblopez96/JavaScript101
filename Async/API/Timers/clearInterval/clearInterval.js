527f0018972612f4ed8733175ba8bcfb93ae88d3
caleblopez96 <caleblopez96@gmail.com>
2025-03-04 18:04:45 -0700

    clarified examples in setInterval and created clearInterval

---
+++ Async/API/Timers/clearInterval/clearInterval.js
@@ -0,0 +1,32 @@
+// clear interval allows you to stop an interval
+
+// SYNTAX:
+// clearInterval(intervalId)
+
+// PARAMETERS:
+// intervalId - id of the interval to clear
+
+// RETURNS:
+// None (undefined)
+
+// EXAMPLE:
+function startCountdown(duration) {
+  const h1 = document.querySelector("h1");
+  let secondsRemaining = duration;
+  const intervalId = setInterval(() => {
+    h1.innerText = secondsRemaining;
+    secondsRemaining -= 1;
+
+    if (secondsRemaining < 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+// REFERENCES:
+
+// COLT STEELE:
+// https://www.udemy.com/course/pro-javascript/learn/lecture/40746850#overview
+
+// MDN:
+// https://www.udemy.com/course/pro-javascript/learn/lecture/40746850#overview
--- Async/API/Timers/setInterval/setInterval.js
+++ Async/API/Timers/setInterval/setInterval.js
@@ -8,6 +8,10 @@
 // function - the function to execute after the delay
 // timeout (optional) - the time in milliseconds that the timer should wait before execution
 
+// RETURNS:
+// an interval id used to clear the interval
+//* its common to capture the id in a variable (intervalId)
+
 // EXAMPLE:
 setInterval(() => {
   console.log("it's been 2 seconds");
@@ -17,7 +21,7 @@ setInterval(() => {
 function startCountdown(duration) {
   const h1 = document.querySelector("h1");
   let secondsRemaining = duration;
-  setInterval(() => {
+  let intervalId = setInterval(() => {
     h1.innerText = secondsRemaining;
     secondsRemaining -= 1;
   }, 1000);
@@ -36,4 +40,9 @@ setTimeout(() => {
 }, 10000);
 
 // REFERENCE:
+
+// COLT STEELE:
+// https://www.udemy.com/course/pro-javascript/learn/lecture/40746846#overview
+
+// MDN:
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/setInterval
