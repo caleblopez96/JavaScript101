6e9257203b02d1459bb15c0af52bfe5e5d0ae080
caleblopez96 <caleblopez96@gmail.com>
2025-03-06 13:34:00 -0700

    removed the random fetch from file

--- Async/API/Local Storage/localStorage.js
+++ Async/API/Local Storage/localStorage.js
@@ -1,31 +1,22 @@
-// The localStorage read-only property of the window interface allows you to access a Storage object for the Document's origin; 
+// The localStorage read-only property of the window interface allows you to access a Storage object for the Document's origin;
 // the stored data is saved across browser sessions.
 
 // SYNTAX:
-localStorage.setItem("key", "value")
-localStorage.getItem("key") // returns value associated with key
-localStorage.removeItem("key") // give key of item to remove
-localStorage.clear() // clears localStorage
+// localStorage.setItem("key", "value")
+// localStorage.getItem("key") // returns value associated with key
+// localStorage.removeItem("key") // give key of item to remove
+// localStorage.clear() // clears localStorage
 
 // EXAMPLE:
 
 // setItem()
-localStorage.setItem("myDog", "B's")
+localStorage.setItem("myDog", "B's");
 
-localStorage.getItem("myDog") // B's
+// getItem()
+localStorage.getItem("myDog"); // B's
 
-localStorage.removeItem("myDog") // 
+// removeItem()
+localStorage.removeItem("myDog"); //
 
+// clear()
 localStorage.clear();
-
-
-fetch("https://pokeapi.co/api/v2/type/3")
-  .then((response) => response.json())
-  .then((data) => {
-    let pokemonArray = data.pokemon;
-
-    let pokemonNames = pokemonArray.map(pokemon => pokemon.pokemon.name)
-
-    console.log(pokemonNames)
-  })
-  .catch(err => console.log("error", err))
