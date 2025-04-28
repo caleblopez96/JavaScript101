6f56ad68b841abbc3fb47bcabb447196381c968a
caleblopez96 <caleblopez96@gmail.com>
2025-04-27 18:17:55 -0700

    Reviewing SOLID OOP principles

--- SOLID OOP/Single Responsibility Principle/SRP.js
+++ SOLID OOP/Single Responsibility Principle/SRP.js
@@ -1,7 +1,8 @@
 // SRP states a class should only have one reason to change.
 // It means that a class should only have one job or responsibility
 
-// Before:
+// Before: this class creates a user with a name and also has a method to serialize the user object to JSON
+// This violates the SRP because the class has two responsibilities: creating a user and serializing it to JSON
 class user {
   constructor(name) {
     this.name = name;
@@ -11,7 +12,8 @@ class user {
   }
 }
 
-// After SRP:
+// After appyling SRP: this class only creates a user with a name and has no other responsibilities
+// The serialization responsibility is moved to a separate class called UserSerializer
 class SRPuser {
   constructor(name) {
     this.name = name;
